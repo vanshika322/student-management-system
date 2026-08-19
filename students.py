bf42e6549eb7ea0053ab46831afd55815c007085
Neha Mishra <nm9566618@gmail.com>
2026-08-19 22:25:21 +0530

    Updated student management system

--- students.py
+++ students.py
@@ -9,3 +9,25 @@ for i in range(0,N):
     l.append(d)
     d={}
 print(l)
+search=input("Enter a roll no. to search students:")
+for d in l:
+    if d["Roll"]==search:
+        print("student found")
+        print(d)
+        break
+search_del=input("enter a roll no. for delete the details:")
+for d in l:
+    if d["Roll"]==search_del:
+        l.remove(d)
+        print("details have deleted")
+        print(l)
+        break
+old_Roll=input("enter a student old roll no.")
+for d in l:
+    if d["Roll"]==old_Roll:
+        New_Roll=input("enter a roll no to update:")
+        d["Roll"]=New_Roll
+        print("roll no. updated successfully")
+        print(d)
+        print(l)
+        break

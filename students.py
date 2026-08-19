f33aaf6ac4095ddc378c45d62d47c69a62a524e8
Neha Mishra <nm9566618@gmail.com>
2026-08-19 23:22:53 +0530

    Updated students.py

--- students.py
+++ students.py
@@ -6,6 +6,7 @@ for i in range(0,N):
     d["Roll"]=input("Enter student's roll no.")
     d["Marks1"]=input("Enter the marks of subject 1:")
     d["Marks2"]=input("Enter the marks of subject 2:")
+    d["branch"]=input("enter branch name:")
     l.append(d)
     d={}
 print(l)
